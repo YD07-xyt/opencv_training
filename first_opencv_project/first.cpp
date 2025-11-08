9343da1749aa4e6b72f3eb917d13819c1ae99f55
YD07-xyt <xyt15080656916@qq.com>
2025-11-08 15:44:15 +0800

    opencv

--- first_opencv_project/first.cpp
+++ first_opencv_project/first.cpp
@@ -1,16 +1,18 @@
 #include<opencv2/opencv.hpp>
-#include <opencv2/imgcodecs.hpp>
+#include <iostream>
 using namespace std;
-int main(){
 
+int main(){
+    // 读取图片
     cv::Mat image= cv::imread("../images/test.jpg");
     int height=image.rows;
     int width=image.cols;
     
     cout << "图片高度(rows):" << height << " 像素" << endl;
     cout << "图片宽度(cols):" << width << " 像素" << endl;
-
+    // 打印图片
     cv::imshow("test",image);
+    // 等待
     cv::waitKey(0);
     return 0;
 }
